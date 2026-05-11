d3ac80a809bc7df282bee2a48471e9d2b27c277f
JoumanaAli <alsagheir.joumana@gmail.com>
2026-05-11 15:38:38 +0300

    Initial Commit

---
+++ macro/macroApp.swift
@@ -0,0 +1,17 @@
+//
+//  macroApp.swift
+//  macro
+//
+//  Created by Joumana Alsagheir on 11/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct macroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
