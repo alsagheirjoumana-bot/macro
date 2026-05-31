a0bf924aa0079b2875607c9d230242a89224a246
JoumanaAli <alsagheir.joumana@gmail.com>
2026-05-31 18:56:47 +0300

    added onboarding view

---
+++ macro/View/OnBoardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnBoardingView.swift
+//  macro
+//
+//  Created by Joumana Alsagheir on 31/05/2026.
+//
+
+import SwiftUI
+
+struct OnBoardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnBoardingView()
+}
